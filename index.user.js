dde1ffb2b491f643921f048b824fba64aa8a8c7d
coderzhao <coderzhaoziwei@outlook.com>
2021-05-12 22:19:12 +0800

    1.0.1

---
+++ index.user.js
@@ -0,0 +1,52 @@
+// ==UserScript==
+// @name         Auto Redirect
+// @namespace    coderzhaoziwei@outlook.com
+// @description  网站外部链接自动跳转 | 知乎 | 简书 | Steam
+// @version      1.0.1
+// @author       Coder Zhao
+// @match        https://www.jianshu.com/go-wild*
+// @match        https://link.zhihu.com/*
+// @match        https://steamcommunity.com/linkfilter*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  "use strict";
+
+  /**
+   * @key paramkey - 查询字串 location.search 中指定的 key
+   * @key hostname - 主机名称 location.hostname 的值
+   * @key testlink - 测试链接 可省略
+   */
+  const list = [
+    {
+      "paramkey": "target",
+      "hostname": "link.zhihu.com",
+      "testlink": "https://link.zhihu.com/?target=https://github.com/coderzhaoziwei",
+    },
+    {
+      "paramkey": "url",
+      "hostname": "www.jianshu.com",
+      "testlink": "https://www.jianshu.com/go-wild?ac=2&url=https://github.com/coderzhaoziwei",
+    },
+    {
+      "paramkey": "url",
+      "hostname": "steamcommunity.com",
+      "testlink": "https://steamcommunity.com/linkfilter/?url=https://github.com/coderzhaoziwei",
+    },
+  ];
+
+  list.find(data => {
+    if (location.hostname === data.hostname) {
+      const params = new URLSearchParams(location.search);
+      const key = data.paramkey;
+
+      if (params.has(key)) {
+        location.replace(params.get(key))
+      }
+
+      return params.has(key);
+    }
+  });
+
+})();
--- readme.md
+++ readme.md
@@ -1,1 +1,16 @@
-# Readme
+# Auto Redirect
+
+当出现以下情况时，脚本将会自动重定向至指定的外部链接。
+
+- 知乎
+![][zhihu-png]
+
+- 简书
+![][jianshu-png]
+
+- Steam
+![][steam-png]
+
+[zhihu-png]: https://cdn.jsdelivr.net/gh/coderzhaoziwei/userscript-auto-redirect/source/zhihu.png
+[jianshu-png]: https://cdn.jsdelivr.net/gh/coderzhaoziwei/userscript-auto-redirect/source/jianshu.png
+[steam-png]: https://cdn.jsdelivr.net/gh/coderzhaoziwei/userscript-auto-redirect/source/steam.png
